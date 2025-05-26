bd83d816a23ae796dc102da1e153fbd6594b397b
iglessias <149502331+iglessias@users.noreply.github.com>
2025-05-26 21:10:26 +0700

    backend new

--- backend/index.js
+++ backend/index.js
@@ -2,10 +2,15 @@ const express = require("express");
 const app = express();
 const port = 3000;
 
+// Middleware untuk parsing JSON
+app.use(express.json());
+
+// Route dasar
 app.get("/", (req, res) => {
-  res.send("Hello xpress!");
+  res.send("Hello World dari backend Express!");
 });
 
+// Menjalankan server
 app.listen(port, () => {
   console.log(`Server berjalan di http://localhost:${port}`);
 });
